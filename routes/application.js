19e82c0620c1ac617c11da868f406ec1a91860b7
sumit oberoi <sumit.oberoi1@gmail.com>
2018-11-27 14:30:01 +0530

    Form rendered

--- routes/application.js
+++ routes/application.js
@@ -3,3 +3,5 @@ const router = express.Router();
 router.get("/new",(req,res) => {
     res.render("applications/new")
 });
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const postRoutes = require("./posts");
 const userRoutes = require("./users");
+const applicationRoutes = require("./application")
 const path = require("path");
 
 const routes = app => {
-  app.use("/application", postRoutes);
+  app.use("/application", applicationRoutes);
   app.use("/users", userRoutes);
   app.get("/about", (req, res) => {
     res.sendFile(path.resolve("static/about.html"));
