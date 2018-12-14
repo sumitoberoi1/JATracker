6bd5caeacb9d49b13aa75903d9fa1175c9bc4f82
sumit oberoi <sumit.oberoi1@gmail.com>
2018-12-13 20:20:43 -0500

    App data linked

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userData = require("../data/users");
 const bcrypt = require('bcrypt');
 const router = express.Router();
-
+let loginData = {layout:false,title:'Login'}
 router.get("/", async (req, res) => {
     if (req.cookies.AuthCookie) {
         if (!req.session.user) {
@@ -13,7 +13,8 @@ router.get("/", async (req, res) => {
             } 
             catch (e) 
             {
-                res.render("login", {layout:false,error: e,title:'Login'});
+                loginData.error = e
+                res.render("login", loginData);
                 return;
             }
             req.session.user = user;
@@ -36,23 +37,24 @@ router.post("/", async (req, res) => {
     } 
     catch (e) 
     {
-        res.render("login", {error: e, layout:false});
+        loginData.error = e
+        res.render("login", loginData);
         return;
 	}
     if (!user) {
-        res.render("login", {layout:false, error: "Username is not valid",title:'Login'});
+        loginData.error =  "Username is not valid"
+        res.render("login", loginData);
         return;
     }
     const passwordsMatch = await bcrypt.compare(password, user.hashedPassword);
     if (passwordsMatch) {
-      const expiresAt = new Date();
-      expiresAt.setHours(expiresAt.getHours() + 1);
-      res.cookie("AuthCookie", user["_id"], { expires: expiresAt });
+      res.cookie("AuthCookie", user["_id"]);
       req.session.user = user;
       res.redirect('/user/view_profile');
     }
     else {
-      res.render("login", {error: "Incorrect login and/or password.",title:'Login'});
+      loginData.error = "Incorrect login and/or password."
+      res.render("login", loginData);
       return;
     }
 
