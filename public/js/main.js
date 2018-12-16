80f2f0396c18aba4ee27223e85ee06986be7395f
ntshwadhwa <44982905+ntshwadhwa@users.noreply.github.com>
2018-12-15 23:30:45 -0500

    updated main.js

--- public/js/main.js
+++ public/js/main.js
@@ -39,12 +39,14 @@ function show_new_work_form() {
     document.getElementById('hiddenNewWorkForm').style.display = "block";
     document.getElementById('coverLetter').style.display = "none";
     document.getElementById('resume').style.display = "none";
+    document.getElementById('projectscard').style.display = "none";
     $('label[for="coverLetter"]').css('display', 'none');
     $('label[for="resume"]').css('display', 'none');   
 }
 
 function hide_new_work_form() {
     document.getElementById('hiddenNewWorkForm').style.display = "none";
+    document.getElementById('projectscard').style.display = "block";
     $('label[for="coverLetter"]').css('display', 'block');
     $('label[for="resume"]').css('display', 'block');
 }
